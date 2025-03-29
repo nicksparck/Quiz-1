8ce94c72708ffad8c1928e53a632e24f296c7948
Torre5_401 <ncruzc2@ucentral.edu.co>
2025-03-28 20:38:51 -0500

    feature(bkenb):Adiciona fecha de registro

---
+++ src/main/java/quiz/universidades/UniversidadesApplication.java
@@ -0,0 +1,13 @@
+package quiz.universidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/quiz/universidades/UniversidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package quiz.universidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
